e89776791cfc05c704c9ed637c6ffb8faab7bdb8
RafaelFernando10 <rsilvao2008@hotmail.com>
2022-05-28 12:54:40 -0300

    Altering method register, adding function for inserting to database

--- project/src/account/AccountController.ts
+++ project/src/account/AccountController.ts
@@ -1,10 +1,21 @@
 import { Request, Response } from "express";
+import prisma from "../database/client";
 
 export class AccountController{
 
     async register(req:Request,res:Response):Promise<void>{
         const {cpf,email,name}=req.body
-        res.json({name,cpf,email})
+
+        const account= await prisma.account.create({
+            data:{
+                name,
+                email,
+                cpf,
+                balance:0
+            }
+        })
+
+        res.json(account)
 
     }
 }
