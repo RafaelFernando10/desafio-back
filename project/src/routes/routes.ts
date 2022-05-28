f134f9ecc27f35afd37ac796f96df07623dd25c9
RafaelFernando10 <rsilvao2008@hotmail.com>
2022-05-28 12:01:40 -0300

    adding import router to app.ts

--- project/src/routes/routes.ts
+++ project/src/routes/routes.ts
@@ -1,12 +1,13 @@
 import { Router } from "express";
-
+import { AccountController } from "../account/AccountController";
 
 const routes = Router();
+const accountController = new AccountController()
 
 routes.post('/register',(req,res)=>accountController.register(req,res))
-routes.get('/searchBalance',(req,res)=>accountController.searchBalance(req,res))
-routes.post('/deposit',(req,res)=>accountController.deposit(req,res))
-routes.post('/withdraw',(req,res)=>accountController.withdraw(req,res))
+//routes.get('/searchBalance',(req,res)=>accountController.searchBalance(req,res))
+//routes.post('/deposit',(req,res)=>accountController.deposit(req,res))
+//routes.post('/withdraw',(req,res)=>accountController.withdraw(req,res))
 
 
 
